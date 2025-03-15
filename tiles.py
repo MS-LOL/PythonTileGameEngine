8b3162d146c0d538724a446a420995e360a8898d
MS-LOL <81468317+MS-LOL@users.noreply.github.com>
2025-03-15 19:32:20 +0000

    started working on modular tile loading code that loads tiles and tile lists from csv

--- tiles.py
+++ tiles.py
@@ -1,6 +1,15 @@
 import pygame, csv, os
 
 class Tile(pygame.sprite.Sprite):
+
+    # Properties:
+    # image
+    # rect
+    #
+    # Methods:
+    # __init__(self, image, x, y, spritesheet)
+    # draw(self, surface)
+
     def __init__(self, image, x, y, spritesheet):
         pygame.sprite.Sprite.__init__(self)
         self.image = spritesheet.parse_sprite(image)
@@ -12,6 +21,24 @@ class Tile(pygame.sprite.Sprite):
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
 class TileMap():
+
+    # Properties:
+    # start_x
+    # start_y
+    # tile_size
+    # spritesheet
+    # tiles
+    # map_surface
+    # map_w
+    # map_h
+    #
+    # Methods:
+    # __init__(self, filename, spritesheet)
+    # draw_map(self, surface)
+    # load_map(self)
+    # read_map_csv(self, filename)
+    # load_tiles(self, filename)
+
     def __init__(self, filename, spritesheet):
         self.tile_size = 16
         self.start_x, self.start_y = 0, 0
@@ -28,7 +55,15 @@ class TileMap():
         for tile in self.tiles:
             tile.draw(self.map_surface)
     
-    def read_csv(self, filename):
+    def read_tile_csv(self, filename):
+        tilesheet = []
+        with open(os.path.join(filename)) as data:
+            data = csv.reader(data, delimiter=',')
+            for row in data:
+                tilesheet.append(list(row))
+        return tilesheet
+
+    def read_map_csv(self, filename):
         map = []
         with open(os.path.join(filename)) as data:
             data = csv.reader(data, delimiter=',')
@@ -43,6 +78,9 @@ class TileMap():
         for row in map:
             x = 0
             for tile in row:
+
+                texturenames = self.read_tile_csv()
+
                 if tile == '0':
                     self.start_x, self.start_y = x * self.tile_size, y * self.tile_size
                 elif tile == '1':
